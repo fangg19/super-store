9c89ac3d82db425d0818b2c1640d4adef258242d
fangg19 <alexjohnny6@gmail.com>
2021-03-14 17:10:22 +0200

    fixing some CSS on Card component

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,21 +1,28 @@
 import classes from './Card.module.css';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Button from './Button';
 import Rating from './Rating';
 
 const Card = (props) => {
   return (
     <div className={classes.Card}>
-      <img src={props.img} alt={props.title} />
+      <img className={classes.ProductImage} src={props.img} alt={props.title} />
       <p>
         <strong>{props.title}</strong>
       </p>
-      <Rating stars={props.rating} />
-      <p>{props.rating}</p>
-      <h2>{props.price}</h2>
-      {props.onSale ? <div className={classes.OnSale}>ON SALE</div> : null}
-      <Button>View Item</Button>
+
+      <div className={classes.Rating}>
+        <Rating stars={props.rating} />
+
+        <p> {props.rating}</p>
+      </div>
+
+      <div className={classes.Price}>
+        <h2>{props.price}</h2>
+        {props.onSale ? <div className={classes.OnSale}>On Sale</div> : null}
+      </div>
+
+      <button className={classes.Btn}>View Item</button>
     </div>
   );
 };
